14cb5b094b019a063d0069fcb1609afe3f33db50
baranirajeshbabu30 <cheersbarani30@gmail.com>
2023-10-18 22:21:45 +0530

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/component/header/header.component';
 import { FooterComponent } from 'src/component/footer/footer.component';
+import { NavbarComponent } from 'src/component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
